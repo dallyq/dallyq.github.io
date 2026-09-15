ffc5eea38a6a4781567c90df6cfc23f7dc655e5d
Dallas Quin <166860264+dallyq@users.noreply.github.com>
2026-09-15 12:26:14 +0200

    Added the show more button for the paragraph for each card.

---
+++ js/script.js
@@ -0,0 +1,12 @@
+function toggleText() {
+  var extraText = document.getElementById("moreText");
+  var buttonText = document.getElementById("showMore");
+
+  if (extraText.style.display === "none") {
+    extraText.style.display = "inline";
+    buttonText.innerHTML = "Show Less";
+  } else {
+    extraText.style.display = "none";
+    buttonText.innerHTML = "Show More";
+  }
+}
